417e0a915998454ab7faea4e2d6f8e53dfe7338b
Evin Padhi <evinpadhi@gmail.com>
2025-11-04 14:11:32 -0800

    Fix variable call

--- scripts/susie_TWAS.R
+++ scripts/susie_TWAS.R
@@ -19,7 +19,9 @@ extract_interval <- function(finemapping_data) {
     interval <- paste0(chromosome,':',start,'-',end)
     interval
 }
-
+# take variants from finemapping results and query GWAS file
+# returns merged dataframe with fine-mapped variants and 
+# gwas summary statistics
 extract_gwas_data <- function(input_susie,input_gwas) {
     require(bedr)
     header <- strsplit(readLines(input_gwas, n = 1),"\t")[[1]]
@@ -44,7 +46,7 @@ extract_gwas_data <- function(input_susie,input_gwas) {
     output
 }
 
-run_TWAS <- function(variant_df,LD_matrix) {
+calculate_TWAS_Z <- function(variant_df,LD_matrix) {
     subset_LD <- LD_matrix[variant_df$variant,variant_df$variant]
     denom <- t(variant_df$posterior_mean) %*% data.matrix(subset_LD) %*%  variant_df$posterior_mean
     stat <- t(variant_df$posterior_mean) %*%  variant_df$z
@@ -55,6 +57,32 @@ run_TWAS <- function(variant_df,LD_matrix) {
     output
 }
 
+TWAS <- function(SusieData,GWAS_path,LD,PhenotypeID) {
+GWAS <- extract_gwas_data(
+                    SusieData,
+                    GWAS_path 
+                    )
+
+# filters gwas data based on number of measurements 
+# for each variant that are present in gwas data. 
+# Sometimes variants have multiple measurements in a gwas 
+# but im not qutie sure why
+FilteredGWAS <- GWAS %>%
+    filter(allele_match == TRUE) %>% 
+    mutate(z = beta/standard_error) %>% 
+    group_by(variant) %>% 
+    filter(dplyr::n() == 1) %>% 
+    ungroup()
+
+ResTWAS <- FilteredGWAS %>% 
+            calculate_TWAS_Z(LD) %>% 
+            mutate(gene = PhenotypeID,
+                   GWAS = tools::file_path_sans_ext(basename(SummaryStats))
+                    )
+ResTWAS
+}
+
+
 ######### PARSE COMMAND LINE ARGUMENTS #########
 option_list <- list(
     optparse::make_option(c("--LDMatrix"), type="character", default=NULL,
@@ -62,7 +90,7 @@ option_list <- list(
     optparse::make_option(c("--PhenotypeID"), type="character", default=NULL,
                         help="Name of gene to compute LD on  ", metavar = "type"),
     optparse::make_option(c("--SummaryStats"), type="character", default=NULL,
-                        help="Path to summary stats for GWAS", metavar = "type"),
+                        help="comma seperated list of Sumstats to use", metavar = "type"),
     optparse::make_option(c("--SusieRes"), type="character", default=NULL,
                         help="Path to finemapping data for a gene", metavar = "type")
     #optparse::make_option(c("--AlleleFrequencies"), type="character", default=NULL,
@@ -77,41 +105,22 @@ OutFileName <- paste0(opt$PhenotypeID,'.TWAS.txt')
 PhenotypeID <- opt$PhenotypeID
 #PathAlleleFrequencies <- opt$AlleleFrequencies
 
+# convert comma seperated list of summary stat files into a dataframe 
+# to loop over and calculate TWAS Z scores 
+#SummaryStatsDf <- data.frame(SummaryStats=strsplit(SummaryStats,','))
 
 ##################### LOAD DATA ##########################
 # loading finemapping data 
 susie_dat <- load_susie_data(FineMappingRes) %>% 
                 mutate(variant = str_replace(variant,'chrchr','chr'))
 
-# take variants from finemapping results and query GWAS file
-# returns merged dataframe with fine-mapped variants and 
-# gwas summary statistics
-GWAS <- extract_gwas_data(
-                    susie_dat,
-                    SummaryStats
-                    )
-
-# filters gwas data based on number of measurements 
-# for each variant that are present in gwas data. 
-# Sometimes variants have multiple measurements in a gwas 
-# but im not qutie sure why
-FilteredGWAS <- GWAS %>%
-    filter(allele_match == TRUE) %>% 
-    mutate(z = beta/standard_error) %>% 
-    group_by(variant) %>% 
-    filter(dplyr::n() == 1) %>% 
-    ungroup()
-
-# reads in LD matrix and filters to the variants that are 
-# present in both studies 
+# Loads LD matrix
 LD <- readRDS(MatrixLD)
-#FilteredLD <- LD[FilteredGWAS$variant,FilteredGWAS$variant]
-
 
 ############# COMPUTE TWAS Z SCORE ########################
-ResTWAS <- FilteredGWAS %>% 
-            run_TWAS(LD) %>% 
-            mutate(gene = PhenotypeID,
-                   GWAS = file_path_sans_ext(basename(SummaryStats))
-                    )
+# computes TWAS Z statistic 
+ResTWAS <-  susie_dat %>%
+            TWAS(SummaryStats,LD,PhenotypeID)
+
+# write to output
 ResTWAS %>% write_tsv(OutFileName)
