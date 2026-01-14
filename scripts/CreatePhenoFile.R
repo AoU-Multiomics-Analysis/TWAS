058f2197d00b5cc32f2cbd56d30267b870749c1c
Evin Padhi <evinpadhi@gmail.com>
2026-01-13 20:42:17 -0800

    init CreatePhenoFile.R

---
+++ scripts/CreatePhenoFile.R
@@ -0,0 +1,34 @@
+library(tidyverse)
+library(data.table) 
+library(optparse)
+
+########### PARSE COMMAND LINE ARGUMENTS ########
+option_list <- list(
+  #TODO look around if there is a package recognizing delimiter in dataset
+  optparse::make_option(c("--BedFile"), type="character", default=NULL,
+                        help="Path ot bed file", metavar = "type"),
+  optparse::make_option(c("--PhenotypeID"), type="character", default=NULL,
+                        help="Name of gene extract  ", metavar = "type")
+    )
+
+opt <- optparse::parse_args(optparse::OptionParser(option_list=option_list))
+BedFilePath<- opt$BedFile
+PhenotypeID <- opt$PhenotypeID
+
+
+############ EXTRACT GENE VALUE ##########
+SubsetBed <- fread(BedFilePath) %>% 
+            dplyr::filter(gene_id == PhenotypeID)
+GeneMeta <- SubsetBed %>% select(1,2,3)
+
+
+PhenoDf <- SubsetBed %>% 
+            dplyr::select(-1,-2,-3) %>%
+            dplyr::pivot_longer(!gene_id) %>% 
+            dplyr::select(-1) %>% 
+            dplyr::mutate(FID =0,IID = name) %>% 
+            dplyr::select(FID,IID,value)
+
+GeneMeta %>% write_tsv('gene_region.tsv')
+PhenoDf %>% write_tsv('pheno.txt')
+
